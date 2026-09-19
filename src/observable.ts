d7d1245aaa39e03594578cc790a7c4ac91f7060b
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-19 08:36:20 +0100

    docs: use scoped GitHub Package imports

--- src/observable.ts
+++ src/observable.ts
@@ -10,7 +10,7 @@
  * @example
  * ```ts
  * import { from } from 'rxjs';
- * import { streamingStats, streamingCorrelation } from 'statistika/observable';
+ * import { streamingStats, streamingCorrelation } from '@diogoribeiro7/statistika/observable';
  *
  * const data$ = from([10, 20, 30, 40, 50]);
  * const stats$ = streamingStats(data$);
